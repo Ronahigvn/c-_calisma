5423de33630b6712723cb56b69fcdd447748a61d
Ronahî <guvenronahi12@gmail.com>
2023-12-06 20:46:00 +0300

    c#
    c# çalışma

---
+++ donguler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazilim Gelistirici Yetistirme Kampı";
+            string kurs2 = "Programlamaya Baslangic Icin Temel Kursu";
+            string kurs3 = "Java Kursu";
+            string kurs4 = "Python Kursu";
+            string kurs5 = "C++ Kursu";
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+            //array-dizi
+            string[] kurslar = new string[] { "Yazilim Gelistirici Yetistirme Kampı" , "Programlamaya Baslangic Icin Temel Kursu" ,"Java Kursu"};
+
+            for (int i = 1; i < kurslar.Length; i ++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("for bitti.");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("Sayfa sonu-footer");
+        }
+    }
+}
---
+++ kodlama.io/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kodlama.io
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi ="Katogori";
+            int ogrenciSayisi=32000;
+            double faizOrani = 1.45;
+            bool sistemeGirişYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+            if(dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalis Butonu");
+            }
+            else if(dolarBugun>dolarDun)
+            {
+                Console.WriteLine("Artis Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi Butonu");
+            }
+            if (sistemeGirişYapmisMi==true)
+            {
+                Console.WriteLine("Kullanici Ayarlari Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+          
+        }
+    }
+}
